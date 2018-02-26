f63d68e0c9a1e58ac411d5251c954fb33744d02d
Utyf <utyff@mail.ru>
2018-02-27 00:45:54 +0300

    trigger fix

--- Src/main.c
+++ Src/main.c
@@ -266,7 +266,7 @@ static void MX_TIM1_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 216;
+  htim1.Init.Prescaler = 108; // 216
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 100;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
