d0a4984382d017bf48b766e1ef2c1a5c31839397
Utyf <utyff@mail.ru>
2018-03-06 00:46:37 +0300

    add core info

--- usr/src/_main.c
+++ usr/src/_main.c
@@ -14,21 +14,63 @@
  * COUNT PER  99 => 10 kHz
  */
 
-void printCPU() {
-    char buf[64];
+void FPUCheck(void) // sourcer32@gmail.com
+{
+    char buf[120];
+    uint32_t mvfr0;
+
+    sprintf(buf, "%08X %08X %08X\n%08X %08X %08X\n",
+           *(volatile uint32_t *)0xE000EF34,   // FPCCR  0xC0000000
+           *(volatile uint32_t *)0xE000EF38,   // FPCAR
+           *(volatile uint32_t *)0xE000EF3C,   // FPDSCR
+           *(volatile uint32_t *)0xE000EF40,   // MVFR0  0x10110021 vs 0x10110221
+           *(volatile uint32_t *)0xE000EF44,   // MVFR1  0x11000011 vs 0x12000011
+           *(volatile uint32_t *)0xE000EF48);  // MVFR2  0x00000040
+    HAL_UART_Transmit(&huart1, (uint8_t*)buf, (uint16_t)strlen(buf), 0xFFFF);
+
+    mvfr0 = *(volatile uint32_t *)0xE000EF40;
+
+    switch(mvfr0)
+    {
+        case 0x10110021 : sprintf(buf, "FPU-S Single-precision only\n"); break;
+        case 0x10110221 : sprintf(buf, "FPU-D Single-precision and Double-precision\n"); break;
+        default : sprintf(buf, "Unknown FPU");
+    }
+    HAL_UART_Transmit(&huart1, (uint8_t*)buf, (uint16_t)strlen(buf), 0xFFFF);
+}
+
+void CORECheck(void) // sourcer32@gmail.com
+{
+    char buf[120];
     uint32_t cpuid = SCB->CPUID;
     uint32_t var, pat;
 
-    uint32_t dev_id = DBGMCU->IDCODE & 0xFFF;
-    sprintf(buf, "\nCPUID %08X DEVID %03X\n", cpuid, dev_id);
+    sprintf(buf, "CPUID %08X DEVID %03X\n", cpuid, DBGMCU->IDCODE & 0xFFF);
     HAL_UART_Transmit(&huart1, (uint8_t*)buf, (uint16_t)strlen(buf), 0xFFFF);
 
     pat = (cpuid & 0x0000000F);
     var = (cpuid & 0x00F00000) >> 20;
-    uint32_t cortex = (cpuid & 0x0000FFF0) >> 4;
-    sprintf(buf, "Cortex M - %X r%dp%d\n", cortex, var, pat);
+
+    if ((cpuid & 0xFF000000) == 0x41000000) // ARM
+    {
+        switch((cpuid & 0x0000FFF0) >> 4)
+        {
+            case 0xC20 : sprintf(buf, "Cortex M0 r%dp%d\n", var, pat); break;
+            case 0xC60 : sprintf(buf, "Cortex M0+ r%dp%d\n", var, pat); break;
+            case 0xC21 : sprintf(buf, "Cortex M1 r%dp%d\n", var, pat); break;
+            case 0xC23 : sprintf(buf, "Cortex M3 r%dp%d\n", var, pat); break;
+            case 0xC24 : sprintf(buf, "Cortex M4 r%dp%d\n", var, pat); break;
+            case 0xC27 : sprintf(buf, "Cortex M7 r%dp%d\n", var, pat); break;
+
+            default : sprintf(buf, "Unknown CORE");
+        }
+    }
+    else
+        puts("Unknown CORE IMPLEMENTER");
     HAL_UART_Transmit(&huart1, (uint8_t*)buf, (uint16_t)strlen(buf), 0xFFFF);
 }
+//****************************************************************************
+
 
 void mainInitialize() {
     DWT_Init();
@@ -39,7 +81,8 @@ void mainInitialize() {
     HAL_TIM_Encoder_Start(&htim8, TIM_CHANNEL_1);
     KEYS_init();
 
-    printCPU();
+    CORECheck();
+    FPUCheck();
 }
 
 void mainCycle() {
