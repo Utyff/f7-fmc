954024a47447731ab7710602ec7d8a63a02a60e9
Utyf <utyff@mail.ru>
2018-03-06 18:28:48 +0300

    small fix

--- usr/inc/_main.h
+++ usr/inc/_main.h
@@ -3,6 +3,18 @@
 
 #include "stm32f7xx_hal.h"
 
+typedef int32_t  s32;
+typedef int16_t s16;
+typedef int8_t  s8;
+
+typedef uint32_t  u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+typedef __IO uint32_t  vu32;
+typedef __IO uint16_t vu16;
+typedef __IO uint8_t  vu8;
+
 extern ADC_HandleTypeDef hadc1;
 extern DMA_HandleTypeDef hdma_adc1;
 extern TIM_HandleTypeDef htim1;
--- usr/inc/lcd_fmc.h
+++ usr/inc/lcd_fmc.h
@@ -4,18 +4,7 @@
 #include "stm32f7xx_hal.h"
 #include "core_cm7.h"
 #include "system_stm32f7xx.h"
-
-typedef int32_t  s32;
-typedef int16_t s16;
-typedef int8_t  s8;
-
-typedef uint32_t  u32;
-typedef uint16_t u16;
-typedef uint8_t  u8;
-
-typedef __IO uint32_t  vu32;
-typedef __IO uint16_t vu16;
-typedef __IO uint8_t  vu8;
+#include <_main.h>
 
 //////////////////////////////////////////////////////////////////////////////////
 //----------------- LCD port definition ----------------
@@ -28,9 +17,11 @@ typedef struct
 } LCD_TypeDef;
 
 // Note: LCD /CS is NE1 - Bank 1 of NOR/SRAM Bank 1~4
+// for A16 as command line for data command
 //#define LCD_BASE           ((uint32_t)(0x60000000 | 0x0001fffE))
 
-// use Bank1.sector1 of NOR / SRAM, address bits HADDR [27,26]=00   A18 as command line for data command
+// use Bank1.sector1 of NOR / SRAM, address bits HADDR [27,26]=00
+// for A18 as command line for data command
 // Note that the STM32 will shift to the right one bit when set!
 #define LCD_BASE        ((u32)(0x60000000 | 0x00007FFFE))
 #define LCD             ((LCD_TypeDef *) LCD_BASE)
