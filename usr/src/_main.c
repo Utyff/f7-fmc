2e14bcb7ab538e8f0fa42c75dd6d22085d4d2367
Utyf <utyff@mail.ru>
2018-03-15 23:40:07 +0300

    use GEN and ENC

--- usr/src/_main.c
+++ usr/src/_main.c
@@ -22,13 +22,13 @@ void mainInitialize() {
     LCD_Init();
 
     //HAL_ADC_Start_DMA(&hadc1, (uint32_t *) samplesBuffer, BUF_SIZE);
-//    ADC_setParams();
+    ADC_setParams();
 
     //HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-//    GEN_setParams();
+    GEN_setParams();
 
-//    HAL_TIM_Encoder_Start(&htim8, TIM_CHANNEL_1);
-//    KEYS_init();
+    HAL_TIM_Encoder_Start(&htim8, TIM_CHANNEL_1);
+    KEYS_init();
     //initScreenBuf();
 
     CORECheck();
